d38f1269216ebba056f0fc4cd4c35dae0dc3ffe6
Aleksey <alekseykasyakov@gmail.com>
2022-12-26 00:25:23 -0700

    create simple login/logout

--- djangoShop/settings.py
+++ djangoShop/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'djangoShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "/"
